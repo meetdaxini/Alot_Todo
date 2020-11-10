ed48ab648bf40378bf41b3b8b723731e1331409d
meetdaxini <meetdaxini10@gmail.com>
2020-11-10 20:17:04 +0530

    crispy forms and footer

--- todo/forms.py
+++ todo/forms.py
@@ -1,8 +1,25 @@
 from django.forms import ModelForm
 from .models import Todo
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
         fields = ['task', 'desc', 'imp']
+
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreateForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+
+
+print(UserCreateForm())
 
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
-from .forms import TodoForm
+from .forms import TodoForm, UserCreateForm
 from .models import Todo
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
@@ -26,7 +26,7 @@ def home(request):
     return render(request, 'todo/index.html', params)
 
 def signupuser(request):
-    form = UserCreationForm()
+    form = UserCreateForm()
     params = {'form': form}
     if request.method == "POST":
         if request.POST['password1'] == request.POST['password2']:
